57aadd059f5ebec36a73712c2fabece29113085d
undertrox <undertrox@gmail.com>
2020-10-22 14:28:32 +0200

    Changed orihime.jar to use a modified version that partially uses english names

--- src/jp/gr/java_conf/mt777/origami/orihime/Text.java
+++ src/jp/gr/java_conf/mt777/origami/orihime/Text.java
@@ -1,18 +1,18 @@
 package jp.gr.java_conf.mt777.origami.orihime;
 
 import jp.gr.java_conf.mt777.origami.dougu.camera.Camera;
-import jp.gr.java_conf.mt777.zukei2d.ten.Ten;
+import jp.gr.java_conf.mt777.zukei2d.ten.Point;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
 
 public class Text {
-    Ten pos;
+    Point pos;
     Camera c;
     int size;
     String text;
 
-    public Text(Ten pos, int size, String text) {
+    public Text(Point pos, int size, String text) {
         this.pos = pos;
         this.size = size;
         this.text = text;
@@ -22,11 +22,11 @@ public class Text {
         this.c = c;
     }
 
-    public Ten getPos() {
+    public Point getPos() {
         return pos;
     }
 
-    public void setPos(Ten pos) {
+    public void setPos(Point pos) {
         this.pos = pos;
     }
 
@@ -48,10 +48,10 @@ public class Text {
 
     public void paint(Graphics2D g) {
 
-        Ten t = new Ten(getPos().getx(), getPos().gety());
-        Ten transformed = c.object2TV(t);
+        Point t = new Point(getPos().getX(), getPos().getY());
+        Point transformed = c.object2TV(t);
         g.setFont(new Font("sans-serif",Font.PLAIN, size));
         g.setColor(Color.BLACK);
-        g.drawString(text, (int) transformed.getx(), (int) transformed.gety());
+        g.drawString(text, (int) transformed.getX(), (int) transformed.getY());
     }
 }
